55ebb7a5341c1ff76faa80c7bfdcb8c424e8b6f4
Melvin <melvingeorge10@gmail.com>
2018-05-08 18:34:16 +0530

    added partial get albums js

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-const token = require('./lib/getToken');
-const keys = require('./spotKeys')||{};
-let token1 = token(keys.clientId,keys.clientSecret).getToken().then(function(res){
+const spotKeys=require('./spotKeys');
+const SpotyJs = require('./main');
+var accToken;
+SpotyJs.keys(spotKeys.clientId,spotKeys.clientSecret).then(function(res){
+    accToken=res.accToken.access_token;
+});
+console.log(accToken);
+SpotyJs.getAlbum("0sNOF9WDwhWunNAHPD3Baj",accToken).then((res)=>{
     console.log(res);
-})
-.catch(function(err){
-    console.log(err);
-});;
-
-    
+});
